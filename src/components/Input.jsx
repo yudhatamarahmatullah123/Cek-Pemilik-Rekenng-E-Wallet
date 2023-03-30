857c5d0271be03a96a4680403c77bc70322a7b45
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-03-31 02:14:44 +0700

    Tambah beberapa parameter dan validasi input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,37 +1,29 @@
 import React, { useState, useEffect } from 'react'
 import { Row, Col, Form, Container } from "react-bootstrap";
-import bankScrapping from "../../data/bankScrapping";
-import fs from "fs";
 
-const Input = () => {
-    const [bank, setBank] = useState([]);
 
-    useEffect(() => {
-        bankScrapping.then((result) => {
-            setBank(result);
-        });
-        // setBank(bankScrapping);
-    }, []);
+const Input = ({ bank, changeAccountBank, accountBank, accountNumber, changeAccountNumber, errorAccount, errorNumber }) => {
     return (
         <div>
             <Container fluid>
-                <Row className="mt-5">
+                <Row className="mt-2">
                     <Col md={6}>
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Label>Account Bank / E-Wallet</Form.Label>
+                        <Form.Group className="mb-3" value={accountBank} onChange={(event) => changeAccountBank(event.target.value)}>
+                            <Form.Label>Bank / E-Wallet</Form.Label>
                             <Form.Select aria-label="Default select example">
                                 <option>Pilih Bank / E-Wallet </option>
                                 {bank && bank.map((data, index) => (
-
-                                    <option value="1" key={index} > {data.nama_bank}</option>
+                                    <option value={data.kode_bank} key={index}> {data.nama_bank}</option>
                                 ))}
                             </Form.Select>
+                            <p className={(errorAccount) ? "text-danger" : "d-none"}><small>Field Bank/E-Wallet harus diisi.</small></p>
                         </Form.Group>
                     </Col>
                     <Col md={6}>
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Label>Account Number</Form.Label>
-                            <Form.Control type="number" placeholder="Nomor rekening bank / E-wallet" />
+                            <Form.Label>Nomor Rekening / E-Wallet</Form.Label>
+                            <Form.Control type="number" value={accountNumber} placeholder="Nomor rekening bank / E-wallet" onChange={(event) => changeAccountNumber(event.target.value)} />
+                            <p className={(errorNumber) ? "text-danger" : "d-none"}><small>Field Nomor Rekening/E-Wallet harus diisi!.</small></p>
                         </Form.Group>
                     </Col>
                 </Row>
