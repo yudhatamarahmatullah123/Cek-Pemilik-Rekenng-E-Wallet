09bd9c1fdbb7459675d2c1083f9e99d291c1aaf8
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-03-31 02:14:13 +0700

    Tambah Author Component di index component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Input from "./Input";
 import TableComponent from "./Table";
 import ButtonComponent from "./Button";
+import AuthorComponent from "./Author";
 
-export { Input, TableComponent, ButtonComponent }
+export { Input, TableComponent, ButtonComponent, AuthorComponent }
