0d6eb49b8998c7a671eac808f2b697056bd8e0ab
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-03-31 02:15:16 +0700

    Tambah alert di table dan validasi table sesuai request

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,33 +1,59 @@
 import React from "react";
-import { Table, Container, Col, Row } from "react-bootstrap";
+import { Table, Container, Col, Row, Alert } from "react-bootstrap";
 
-const TableComponent = () => {
-    return (
-        <>
-            <Container fluid>
-                <Row>
-                    <Col md={12} className="mt-5">
-                        <h4 className="text-center">Hasil Pencarian</h4>
-                        <Table striped bordered hover className="text-center">
-                            <thead>
-                                <tr>
-                                    <th>Nomor Rekening</th>
-                                    <th>Nama Pemilik</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>11111111</td>
-                                    <td>Mark</td>
-                                </tr>
-
-                            </tbody>
-                        </Table>
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    )
+const TableComponent = ({ result, jenis }) => {
+    if (result) {
+        let hasil = JSON.parse(result);
+        let status = hasil.status
+        return (
+            <>
+                <Container fluid>
+                    <Row>
+                        <Col md={12} className="mt-3">
+                            <h4 className="text-center">Hasil Pencarian</h4>
+                            <AlertComponent status={status} jenis={jenis} />
+                            <TableData status={status} data={hasil.data} jenis={jenis} />
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        )
+    }
 }
 
+const AlertComponent = ({ status, jenis }) => {
+    if (status === "failed") {
+        return (
+            <div className="text-center">
+                <Alert variant="danger">
+                    {(jenis == "bank") ? "Nomor Rekening Tidak Ditemukan!." : "Nomor E-Wallet Tidak Ditemukan / Terkena Limit!."}
+                </Alert>
+            </div>
+        );
+    }
+
+}
+const TableData = ({ status, data, jenis }) => {
+    if (status == "success") {
+        return (
+            <Table striped bordered hover className="text-center">
+                <thead>
+                    <tr>
+                        <th>{(jenis == "bank") ? "Bank" : "E-Wallet"}</th>
+                        <th>Nomor {(jenis == "bank") ? "Rekening" : "E-Wallet"}</th>
+                        <th>Nama Pemilik</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{(jenis == "bank") ? data.bank_name : data.ewallet_name}</td>
+                        <td>{(jenis == "bank") ? data.account_number : data.phone_number}</td>
+                        <td>{data.customer_name}</td>
+                    </tr>
+
+                </tbody>
+            </Table>
+        );
+    }
+}
 export default TableComponent
